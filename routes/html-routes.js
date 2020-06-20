4ec3c83fc182bdfb34bec70d933f73fe6a8f78d8
daniel molina <bigdpower@icloud.com>
2020-06-20 13:23:55 -0700

    signup and login our now running

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,24 +6,28 @@ const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 
 module.exports = function(app) {
-  app.get("/", (req, res) => {
+  app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("members");
     }
-    res.render("index")
+    res.render("signup");
   });
 
-  app.get("/login", (req, res) => {
+  app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("members");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.render("login");
   });
-  app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("members", isAuthenticated, function(req, res) {
+    res.render("members");
   });
+  
   app.get("/mood", function(req, res) {
     let query;
     if (req.query.mood) {
