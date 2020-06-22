bac336e24f096a481a79b8dfcfaa8fac550d04c8
daniel molina <bigdpower@icloud.com>
2020-06-22 15:27:10 -0700

    travis cli errors fixed

--- public/js/playpage.js
+++ public/js/playpage.js
@@ -1,10 +1,11 @@
 $(document).ready(() => {
-  $("[data-rating] .star").on("click", function(){
+  $("[data-rating] .star").on("click", function() {
     const selectedCssClass = "selected";
     const $this = $(this);
     $this.siblings("." + selectedCssClass).removeClass(selectedCssClass);
     $this
-    .addClass(selectedCssClass)
-    .parent().addClass("is-voted");
-});
+      .addClass(selectedCssClass)
+      .parent()
+      .addClass("is-voted");
+  });
 });
